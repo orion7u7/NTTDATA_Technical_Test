b7cead5db7f15ba8977ab2880440867de9a18239
Orion Guevara <oguevaracorredor@gmail.com>
2023-06-10 02:51:25 -0500

    Config server

--- ConfigService/src/main/java/com/nttdata/configservice/ConfigServiceApplication.java
+++ ConfigService/src/main/java/com/nttdata/configservice/ConfigServiceApplication.java
@@ -2,9 +2,11 @@ package com.nttdata.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServiceApplication {
 
     public static void main(String[] args) {
